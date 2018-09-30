de61b14a4edf6c3e8ea81c171d96415f6d3ee20c
xuyin <1499978024@qq.com>
2018-09-30 17:32:44 +0200

    Interfaces graphiques

--- src/InterfaceGraphiques/Interface.java
+++ src/InterfaceGraphiques/Interface.java
@@ -1,4 +1,16 @@
 package InterfaceGraphiques;
 
+import java.awt.*;
+
 public class Interface {
+    public static void main(String[] args){
+        FabriqueComposant fabrique=new FabriqueComposantMacOS();
+        Bouton bouton=fabrique.creeBouton("rouge",3,4);
+        bouton.afficheCarateristiques();
+
+        fabrique = new FabriqueComposantLinux();
+        Menu menu = fabrique.creeMenu("rouge", 7);
+        menu.afficheCarateristiques();
+
+    }
 }
